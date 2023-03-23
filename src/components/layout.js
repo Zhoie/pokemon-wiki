99b73216b07b12848db1b536e4c26450fc5d8a58
Niklas <zhoie@icloud.com>
2023-03-24 00:11:38 +0800

    colors

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,7 @@ import Link from 'next/link'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
-
 export default function Layout({ children }) {
-
     const router = useRouter()
 
     return(
@@ -12,16 +10,14 @@ export default function Layout({ children }) {
             <Head>
                 <title>Pokemon</title>
             </Head>
-
             <h1 className="nav-title">Pokemon Dex</h1>
-
             <nav className="header-nav">
                 <ul>
                     <li><Link href="/">home</Link></li>
                 </ul>
-
             </nav>
-            { children}
+
+            { children }
         </>
     )
 }
