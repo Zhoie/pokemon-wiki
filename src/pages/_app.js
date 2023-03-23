c87c70a709acd7d68d70da1444637037e3f168bd
Niklas <zhoie@icloud.com>
2023-03-23 15:24:22 +0800

    images

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import Link from "next/link";
+import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    
+  )
 }
